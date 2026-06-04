e035acd99f8f14ea5da6f9926edb86ef25144bcf
Niels Ouvrard Sii <niels.ouvrard@sii.fr>
2026-06-04 12:08:24 +0200

    clean

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,28 @@
 #![no_main]
 
 use aarch32_rt::entry;
-use qemu_be_error as _;
+use cortex_r5_emul as _;
 use semihosting::println;
+
+#[derive(PartialEq)]
+#[repr(u8)]
+pub enum MyEnum {
+    A,
+    B,
+    C,
+    D,
+}
 
 /// The entry-point to the Rust application.
 ///
 /// It is called by the start-up.
 #[entry]
 fn my_main() -> ! {
-    qemu_be_error::init();
+    cortex_r5_emul::init();
     let x = 1.0f64;
     let y = x * 2.0;
     println!("Hello, this is semihosting! x = {:0.3}, y = {:0.3}", x, y);
-    qemu_be_error::want_panic();
+    cortex_r5_emul::want_panic();
 
     let a = MyEnum::A;
     let _b = MyEnum::B;
@@ -30,24 +39,4 @@ fn my_main() -> ! {
     println!("{cat}");
 
     panic!("I am an example panic");
-}
-
-#[derive(PartialEq)]
-#[repr(u8)]
-pub enum MyEnum {
-    A,
-    B,
-    C,
-    D,
-}
-
-fn funct() -> u32 {
-    let a = MyEnum::A;
-    let _b = MyEnum::B;
-
-    if a == MyEnum::B {
-        0
-    } else {
-        1
-    }
 }
